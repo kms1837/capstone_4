5806250b61b33f6c5c4645ff88f1029476c076de
kms1837 <kms1837@gmail.com>
2019-05-17 15:14:46 +0900

    add user, department sample query file

---
+++ sample/department.sql
@@ -0,0 +1,5 @@
+INSERT INTO department VALUES(1, 1, "컴퓨터공학");
+INSERT INTO department VALUES(2, 1, "소프트웨어");
+INSERT INTO department VALUES(3, 1, "정보보호");
+INSERT INTO department VALUES(4, 1, "데이터사이언스");
+INSERT INTO department VALUES(5, 1, "지능기전");
