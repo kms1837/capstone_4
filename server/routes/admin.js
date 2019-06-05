69fd5e2f8e901af7b9520020e7206e4488bec66e
kms1837 <kms1837@gmail.com>
2019-06-05 22:45:20 +0900

    update python runn code

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const pythonShell = require('python-shell').PythonShell;
 const userModel = require('../models/user');
 const trackModel = require('../models/track');
 const studentTrackModel = require('../models/student_track');
 const stuSpecModel = require('../models/stu_spec');
 const finalScoreModel = require('../models/final_score');
 const common = require('./common');
+const pythonConfig = require('../config/index').pythonConfig;
 const router = express.Router();
 
 router.get('/', async(request, response) => {
@@ -58,6 +60,19 @@ router.get('/accept_spec', async(request, response) => {
 
 router.put('/accept_spec/:specID/agree', async(request, response) => {
     await stuSpecModel.agree(request.params.specID);
+    let specInfo = await stuSpecModel.findToID(request.params.specID);
+    let studentID = specInfo[0]["student_id"];
+
+    var options = {
+      ...pythonConfig,
+      args: [studentID]
+    };
+    
+    pythonShell.run('update_user_data.py', options, (err, results) => {
+      if (err) throw err;
+      console.log('results: ', results);
+    });
+
     response.status(200).send(true);
 });
 
